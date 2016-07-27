93537ce69e8671974c8ad971018073cac102ef99
Hackbright Student <info@hackbrightacademy.com>
2016-07-26 17:04:50 -0700

    Added new routes and templates

--- hackbright-web.py
+++ hackbright-web.py
@@ -4,14 +4,41 @@ import hackbright
 
 app = Flask(__name__)
 
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
 
 @app.route("/student")
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
-    return "%s is the GitHub account for %s %s" % (github, first, last)
+    return render_template("student_info.html", 
+                           github=github, 
+                           first=first, 
+                           last=last)
+
+@app.route("/student-add-form")
+def student_add_form():
+    """Form to add a new student."""
+
+    return render_template("student_add.html")
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+
+    first = request.form.get("first")
+    last = request.form.get("last")
+    github = request.form.get("github")
+
+    # QUERY statements goes here
+
+    return render_template("student_new.html")
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
